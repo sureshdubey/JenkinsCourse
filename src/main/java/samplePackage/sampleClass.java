da4cae205a67a436dac0ea5f6cc93332ff77a1c7
SURESH DUBEY <suresh.dubey@allianz.com>
2018-04-29 17:04:58 +0530

    sample maven project

---
+++ src/main/java/samplePackage/sampleClass.java
@@ -0,0 +1,12 @@
+package samplePackage;
+
+import junit.framework.Assert;
+
+public class sampleClass {
+
+	public static void main(String[] args) {
+		String name = "Suresh Dubey";
+		Assert.assertEquals(name, "Suresh Dubey");
+	}
+
+}
